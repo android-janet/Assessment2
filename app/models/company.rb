17229045479366f3749d8d95c0a45bacc5d483f3
Melissa Khat <melissa.khat@gmail.com>
2015-05-19 04:52:06 -0600

    model validations, doublechecking index and show pages

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,7 @@ class Company < ActiveRecord::Base
 
 has_and_belongs_to_many :industries
 
+validates :name, :description, :price, presence: true
+validates :industry_id, uniqueness: true
+
 end
--- app/models/industry.rb
+++ app/models/industry.rb
@@ -2,4 +2,7 @@ class Industry < ActiveRecord::Base
 
 has_and_belongs_to_many :companies
 
+validates :name, :description, presence: true
+validates :company_id, uniqueness: true
+
 end
